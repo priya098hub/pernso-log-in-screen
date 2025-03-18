a90ccebec6f31d85b7882c43ec8d1ad2229819d2
priya098hub <priyathori97@gmail.com>
2025-03-18 12:06:55 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:ludo_pernso/screens/home_screen.dart';
+import 'package:ludo_pernso/screens/login_screen.dart';
+import 'package:ludo_pernso/screens/signup_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+  runApp(MyApp(isLoggedIn: isLoggedIn));
+}
+
+class MyApp extends StatelessWidget {
+  final bool isLoggedIn;
+  const MyApp({super.key, required this.isLoggedIn});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ludo Pernso',
+      debugShowCheckedModeBanner: false,
+      initialRoute: isLoggedIn ? '/home' : '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
+    );
+  }
+}
